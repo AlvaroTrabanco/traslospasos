431a52209becda8e822e462513acf285d42714ab
AlvaroTrabanco <alvaro.suarez.trabanco@gmail.com>
2017-06-07 15:32:02 +0100

    System of 9 columns. All the text have the same style as the placeholder %text. The text can be aligned with any column in the desktop version. The images have a similar system about its width and position

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
   console.log("start!");
-
-
-
-
-
-
+/*clearfix from right alignment*/
+  $('.text-right').after('<div class="clearfix"></div>');
+  $('.text-far-right').after('<div class="clearfix"></div>');
+  $('.text-far-far-right').after('<div class="clearfix"></div>');
+  $('.single-img-right').after('<div class="clearfix"></div>');
+  $('.single-img-far-right').after('<div class="clearfix"></div>');
+  $('.single-img-far-far-right').after('<div class="clearfix"></div>');
 
 
 
