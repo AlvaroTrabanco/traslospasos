67c1f094609ae074207e826008e04f4e7cdaffb0
AlvaroTrabanco <alvaro.suarez.trabanco@gmail.com>
2017-06-29 13:00:32 +0100

    final audio system developed

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,37 @@
 $(document).ready(function(){
-
   console.log("start!");
 
+  /*Audio with text*/
+  var $firstAudio = $("#firstAudio");
+  var $secondAudio = $("#secondAudio");
+  var $thirdAudio = $("#thirdAudio");
+  var $firstAudioMarker = $("#firstAudioMarker")
+  var $secondAudioMarker = $("#secondAudioMarker");
+  var $thirdAudioMarker = $("#thirdAudioMarker");
+  var $exitingText = $("#stopAudio");
+
+  $firstAudioMarker.waypoint(function(){
+    $firstAudio.get(0).play();
+    $firstAudio.animate({volume: 1}, 2000);
+  }, {offset: "bottom-in-view"}
+  );
+  $secondAudioMarker.waypoint(function(){
+    $secondAudio.get(0).play();
+    $secondAudio.animate({volume: 1}, 2000);
+  }, {offset: "bottom-in-view"}
+  );
+  $thirdAudioMarker.waypoint(function(){
+    $thirdAudio.get(0).play();
+    $thirdAudio.animate({volume: 1}, 2000);
+  }, {offset: "bottom-in-view"}
+  );
+
+  $exitingText.waypoint(function(){
+    $firstAudio.animate({volume: 0}, 2000);
+    $secondAudio.animate({volume: 0}, 2000);
+    $thirdAudio.animate({volume: 0}, 2000);
+  });
+  /*until here*/
 
 
 
@@ -9,6 +39,17 @@ $(document).ready(function(){
 
 
 
+
+
+
+
+
+
+
+
+
+
+  /*Fadein/out gallery according to a certain word*/
   var $markerFadeImg1 = $("#markerFadeImg1");
   var $singleImgFade1 = $("#imgFadeIntro1");
   var $singleImgFade2 = $("#imgFadeIntro2");
@@ -106,15 +147,22 @@ $(document).ready(function(){
             }
           }, {offset: "-20%"});
         });
-
-
-
-      }
+    }
   }
 
+  // run test on initial page load
+  checkSize();
+
+  // run test on resize of the window
+  $(window).resize(checkSize);
+/*until here*/
 
 
 
+
+
+
+/*Code for fullscreen gallery controlled by blocks with text inside*/
   var $exFsGalleryControllerEl1 = $("#exFsGalleryControllerEl1");
   var $exFsGalleryControllerEl2 = $("#exFsGalleryControllerEl2");
   var $exFsGalleryControllerEl3 = $("#exFsGalleryControllerEl3");
@@ -169,15 +217,14 @@ $(document).ready(function(){
       $exGalleryEl3.addClass('active');
     }
   }, {offset: "-100%"} );
+  /*until here*/
 
 
 
-
+  /*Fit images in fullscreen*/
   var $windowHeight = $(window).height();
   var $bgImg = $(".bgFsImg img");
   var $bgImgHeight = $(".bgFsImg img").height();
-
-
 
   function correctBg(){
     if ($windowHeight > $bgImgHeight) {
@@ -188,7 +235,8 @@ $(document).ready(function(){
     }
   }
 
-correctBg();
+  correctBg();
+  /*until here*/
 
 
 
@@ -198,26 +246,9 @@ correctBg();
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var cuencasMinerasHeight = $("#cuencasMineras").height();
-$(".bgControllerEl").css("height", cuencasMinerasHeight/4);
-
-
-
+  /*Fullscreen background gallery*/
+  var cuencasMinerasHeight = $("#cuencasMineras").height();
+  $(".bgControllerEl").css("height", cuencasMinerasHeight/4);//this number should change according to the number of images you want to display
 
   var $cuencasMinerasController1 = $("#cuencasMinerasController1");
   var $cuencasMinerasController2 = $("#cuencasMinerasController2");
@@ -297,55 +328,8 @@ $(".bgControllerEl").css("height", cuencasMinerasHeight/4);
     }
   }, {offset: function() {
     return -this.element.clientHeight} } );
+/*until here*/
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // run test on initial page load
-  checkSize();
-
-  // run test on resize of the window
-  $(window).resize(checkSize);
 
 
 
@@ -359,16 +343,11 @@ $(".bgControllerEl").css("height", cuencasMinerasHeight/4);
   $('.single-img-right').after('<div class="clearfix"></div>');
   $('.single-img-far-right').after('<div class="clearfix"></div>');
   $('.single-img-far-far-right').after('<div class="clearfix"></div>');
+/*until here*/
 
 
 
-  // $(".imgMarker1, .imgMarker2").each(function() {
-  //    $( this ).waypoint(function() {
-  //      $( this ).siblings(".legend").toggleClass("active");
-  //      console.log("working");
-  //    }, {offset: "20%"});
-  // });
-
+// legend stuff. Needs revision
 
 var $imgMarker1 = $("#imgMarker1");
 $imgMarker1.waypoint(function() {
@@ -380,21 +359,19 @@ $imgMarker2.waypoint(function() {
   $("#imgMarker2").siblings(".legend").toggleClass("active");
 }, {offset: "20%"});
 
-//
-// $('[id^=imgMarker]').each(function() {
-//   var number = this.id.split('_').pop();
-// });
 
 
 
+
+//timeline stuff
 var $timelineMarker1 = $("#timelineMarker1");
 var $timelineFirst = $("#timeline-first");
-var $timelineMarker2 = $("#timelineMarker2");
-var $timelineSecond = $("#timeline-second");
-var $timelineMarker3 = $("#timelineMarker3");
-var $timelineThird = $("#timeline-third");
-var $timelineMarker4 = $("#timelineMarker4");
-var $timelineFourth = $("#timeline-fourth");
+// var $timelineMarker2 = $("#timelineMarker2");
+// var $timelineSecond = $("#timeline-second");
+// var $timelineMarker3 = $("#timelineMarker3");
+// var $timelineThird = $("#timeline-third");
+// var $timelineMarker4 = $("#timelineMarker4");
+// var $timelineFourth = $("#timeline-fourth");
 var $vigneteTimeline = $("#vignete");
 
 $timelineMarker1.waypoint(function(direction) {
@@ -418,81 +395,7 @@ $timelineMarker1.waypoint(function(direction) {
     $vigneteTimeline.fadeOut(300);
   }
 }, {offset: "100px"});
-
-
-//
-// $timelineMarker2.waypoint(function(direction) {
-//   if(direction == "down"){
-//     $timelineSecond.fadeIn(1000);
-//     $vigneteTimeline.fadeIn(1000);
-//     console.log("I should be seen");
-//   } else{
-//     $timelineSecond.fadeOut(400);
-//     $vigneteTimeline.fadeOut(500);
-//     console.log("I should have disappeared");
-//   }
-// }, {offset: "70%"} );
-//
-// $timelineMarker2.waypoint(function(direction) {
-//   if(direction == "up"){
-//     $timelineSecond.fadeIn(500);
-//     $vigneteTimeline.fadeIn(500)
-//   }else{
-//     $timelineSecond.fadeOut(400);
-//     $vigneteTimeline.fadeOut(500);
-//   }
-// }, {offset: "100px"});
-//
-//
-//
-// $timelineMarker3.waypoint(function(direction) {
-//   if(direction == "down"){
-//     $timelineThird.fadeIn(1000);
-//     $vigneteTimeline.fadeIn(1000);
-//     console.log("I should be seen");
-//   } else{
-//     $timelineThird.fadeOut(400);
-//     $vigneteTimeline.fadeOut(500);
-//     console.log("I should have disappeared");
-//   }
-// }, {offset: "70%"} );
-//
-// $timelineMarker3.waypoint(function(direction) {
-//   if(direction == "up"){
-//     $timelineThird.fadeIn(500);
-//     $vigneteTimeline.fadeIn(500)
-//   }else{
-//     $timelineThird.fadeOut(400);
-//     $vigneteTimeline.fadeOut(500);
-//   }
-// }, {offset: "100px"});
-//
-//
-//
-// $timelineMarker4.waypoint(function(direction) {
-//   if(direction == "down"){
-//     $timelineFourth.fadeIn(1000);
-//     $vigneteTimeline.fadeIn(1000);
-//     console.log("I should be seen");
-//   } else{
-//     $timelineFourth.fadeOut(400);
-//     $vigneteTimeline.fadeOut(500);
-//     console.log("I should have disappeared");
-//   }
-// }, {offset: "70%"} );
-//
-// $timelineMarker4.waypoint(function(direction) {
-//   if(direction == "up"){
-//     $timelineFourth.fadeIn(500);
-//     $vigneteTimeline.fadeIn(500)
-//   }else{
-//     $timelineFourth.fadeOut(400);
-//     $vigneteTimeline.fadeOut(500);
-//   }
-// }, {offset: "100px"});
-
-
-/*fade in out single images*/
+//until here
 
 
 
@@ -507,6 +410,7 @@ $timelineMarker1.waypoint(function(direction) {
 
 
 
+//menu beginning
 
 var Menu = {
 
@@ -544,11 +448,6 @@ Menu.init();
 
 
 
-
-
-/*menu...*/
-
-
   $('#articlesMenuRight').hover(function(){
     $('#articlesMenuRight').toggleClass('down');
     $('#backgroundColor').toggleClass('darkBackground');
@@ -583,13 +482,13 @@ function hideMenu(){
 hideMenu();
 $(window).resize(hideMenu);
 
+/*until here*/
 
 
 
 
 
-
-//init ScrollMagic
+/*pinning video*/
 
   var controller = new ScrollMagic.Controller();
 
@@ -619,10 +518,10 @@ $(window).resize(hideMenu);
     intent: "200"
   })
   .addTo(controller);
+/*until here*/
 
 
-
-
+/*pinned images*/
 $(function() {
     var controller = new ScrollMagic.Controller();
     $(".imgpin").each(function () {
@@ -641,9 +540,11 @@ $(function() {
       });
     });
 });
+/*until here*/
 
 
 
+/*youtube video playing when visible*/
 window.onYouTubePlayerAPIReady = function(){
 
   var LoadVideo = function(player_id){
@@ -708,39 +609,15 @@ window.onYouTubePlayerAPIReady = function(){
   LoadVideo('playerA');
   LoadVideo('playerB');
 
-  // LoadVideo('playerC');
-
-// or (loop mode)
-  // $('#video').each(function(){
-  // 	LoadVideo($(this).attr('#playerA'));
-  // });
 }
+/*until here*/
 
 
 
 
-
-/*lazy load*/
-
-
-
-  $(function() {
-      $(".lazy").recliner({
-          attrib: "data-src", // selector for attribute containing the media src
-          throttle: 300,      // millisecond interval at which to process events
-          threshold: 500,     // scroll distance from element before its loaded
-          live: true          // auto bind lazy loading to ajax loaded elements
-      });
-  });
 
 
 
 
 
 });
-
-
-
-var event = function(){
-  $(this).addClass('articlesInteraction');
-}
